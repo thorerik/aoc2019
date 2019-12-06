d8da34e5ed185c2c9ceeb23ece8ff9225659a05c
Thor Erik Lie <thor@thorerik.com>
2019-12-06 02:49:09 +0100

    Add class template

---
+++ src/Days/Day.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: thor
+ * Date: 06.12.19
+ * Time: 00:08
+ */
+
+namespace Thor\AdventOfCode\Days;
+
+
+use Thor\AdventOfCode\Exceptions\NotImplementedException;
+
+class Day1
+{
+    public function computeResult(array $testCases): int {
+        throw new NotImplementedException();
+        $result = 0;
+
+        return $result;
+    }
+
+    public function computeResultPt2(array $testCases): int {
+        throw new NotImplementedException();
+        $result = 0;
+
+        return $result;
+    }
+}
